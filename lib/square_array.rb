050bb94522ee8b11a44ec61b302da6d62eca6810
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-17 01:14:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,5 +3,6 @@ def square_array(numbers)
   counter = 0
   square_numbers = []
   while numbers[counter] do
-    square_numbers.push(numbers[counter]) = 
+    square_numbers.push(numbers[counter])
+    
 end
