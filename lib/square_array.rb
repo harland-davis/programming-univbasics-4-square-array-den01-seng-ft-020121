a7a8d9a57130bfb08a4d46487c5a56f7407719e6
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-17 00:59:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(numbers)
   count = 0
   new_array = []
   while count < numbers.length do
-    new_array.push(numbers)
+    new_array.push(numbers[count])
     count += 1
   end
 end
